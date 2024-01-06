4ef478cbae233d925ee8331f4fbd71ae6bac6c3a
Jens Larsson <Larssonjenz@gmail.com>
2024-01-06 12:41:13 +0100

    feat: add part 2 of day1

--- src/Day01/main.c
+++ src/Day01/main.c
@@ -2,47 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-void process_file(const char *filename)
+// array of number strings
+const char *numbers[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+// instead of replacing the entire , just replace the middle character of the word with the number
+void replace(char *str, const char *sub, const char rep)
 {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL)
+    char *pos = strstr(str, sub);
+    do
     {
-        perror("Error opening file");
-        exit(EXIT_FAILURE);
-    }
-
-    char line[1024];
-    int sum = 0;
-    while (fgets(line, sizeof(line), file))
-    {
-        printf("%s", line);
-        char number[3] = {'\0', '\0', '\0'};
-        int lineLength = strlen(line);
-
-        for (int i = 0; i < lineLength; i++)
+        if (pos != NULL)
         {
-            if (line[i] >= '0' && line[i] <= '9')
-            {
-                number[0] = line[i];
-                printf("%c \n", line[i]);
-                break;
-            }
+            int subLen = strlen(sub);
+            pos[subLen / 2] = rep;
         }
-        for (int i = lineLength - 1; i >= 0; i--)
-        {
-            if (line[i] >= '0' && line[i] <= '9')
-            {
-                number[1] = line[i];
-                printf("%c \n", line[i]);
-                break;
-            }
-        }
-        sum += atoi(number);
-        printf("%d\n", atoi(number));
-    }
-    printf("Sum: %d\n", sum);
-
-    fclose(file);
+        pos = strstr(str, sub);
+    } while (pos != NULL);
 }
 
 int main(int argc, char *argv[])
@@ -53,7 +28,58 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    process_file(argv[1]);
+    // open file
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    // For each line in the file
+    char line[1024];
+    int sum = 0;
+    while (fgets(line, sizeof(line), file))
+    {
+        printf("%s \n", line);
+        for (int i = 0; i < 10; i++)
+        {
+            // insert number into the words which correspond to the number, effectively replacing the word
+            replace(line, numbers[i], i + '0');
+        }
+        printf("%s \n", line);
+
+        char number[3] = {'\0', '\0', '\0'};
+        int lineLength = strlen(line);
+
+        // find first number
+        for (int i = 0; i < lineLength; i++)
+        {
+            if (line[i] >= '0' && line[i] <= '9')
+            {
+                number[0] = line[i];
+                break;
+            }
+        }
+
+        // find last number
+        for (int i = lineLength - 1; i >= 0; i--)
+        {
+            if (line[i] >= '0' && line[i] <= '9')
+            {
+                number[1] = line[i];
+                break;
+            }
+        }
+
+        // convert to int and add to sum
+        int endNumber = atoi(number);
+        sum += endNumber;
+        printf("end number: %d\n", endNumber);
+    }
+
+    printf("Sum: %d\n", sum);
+    fclose(file);
 
     return 0;
 }
